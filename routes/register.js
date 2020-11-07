8e698c7e55038435360c8e7966bab3464fa97d16
yoonseokch <yoonsukch@gmail.com>
2020-11-07 20:55:49 +0900

    phone redundancy check

--- routes/register.js
+++ routes/register.js
@@ -45,50 +45,63 @@ router.post('/check',(req,res) => {
         res.json({success:false});
       }
     });
-  });
+});
 router.post('/phone-validate',(req,res) => {
   console.log(process.env.AWS_SDK_LOAD_CONFIG);
   console.log(AWS.config.credentials);
-  if(req.body.phone) {
-    let phoneNum = req.body.phone.replace(/-/g, "");
-
-    let randomNum = ((min, max) => {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    })(1000, 9999);
-
-    if(req.body.phone in validateNum) {
-      clearTimeout(validateNum[req.body.phone][1]);
-      validateNum[req.body.phone][0] = randomNum;
-    }else {
-      validateNum[req.body.phone] = [randomNum, -1];
+  db.User.User.findAll({
+    where : {
+      phone :req.body.phone
     }
-
-    validateNum[req.body.phone][1] = setTimeout(() => { 
-      if(req.body.phone in validateNum) {
-        delete validateNum[req.body.phone];
+  }).then((data)=>{
+    if (data.length==0)
+    {
+      if(req.body.phone) {
+        let phoneNum = req.body.phone.replace(/-/g, "");
+    
+        let randomNum = ((min, max) => {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        })(1000, 9999);
+    
+        if(req.body.phone in validateNum) {
+          clearTimeout(validateNum[req.body.phone][1]);
+          validateNum[req.body.phone][0] = randomNum;
+        }else {
+          validateNum[req.body.phone] = [randomNum, -1];
+        }
+    
+        validateNum[req.body.phone][1] = setTimeout(() => { 
+          if(req.body.phone in validateNum) {
+            delete validateNum[req.body.phone];
+          }
+        }, 180000);
+    
+        let params = {
+          Message: "[로우봇] 인증번호 [" + randomNum.toString() + "]를 입력해주세요.",
+          PhoneNumber: '+82' + phoneNum,
+        };
+    
+        const publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+    
+        publishTextPromise.then(
+          function(data) {
+            res.json({ success: true });
+          }).catch(
+            function(err) {
+              console.error(err, err.stack);
+              res.json({ success: false });
+          });
+      }else {
+        res.json({ success: false });
       }
-    }, 180000);
-
-    let params = {
-      Message: "[로우봇] 인증번호 [" + randomNum.toString() + "]를 입력해주세요.",
-      PhoneNumber: '+82' + phoneNum,
-    };
-
-    const publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-
-    publishTextPromise.then(
-      function(data) {
-        res.json({ success: true });
-      }).catch(
-        function(err) {
-          console.error(err, err.stack);
-          res.json({ success: false });
-      });
-  }else {
-    res.json({ success: false });
-  }
+    }
+    else
+    {
+      res.json({success:false,msg:"이미 있는 휴대폰 번호입니다"});
+    }
+  })
 });
 router.post('/phone-check', (req, res) => {
   if(req.body.phone && req.body.phone in validateNum) {
